b7702bb3ce402ecb38503ff68405f205f0100cfa
Nolan Lawson <nolan@nolanlawson.com>
2025-09-14 14:21:22 -0700

    Revert "improve impl"
    This reverts commit 607f5bcc59c201ab4584b95077151dea47f89a52.

--- src/lib/defineEventHandlerIDLAttribute.ts
+++ src/lib/defineEventHandlerIDLAttribute.ts
@@ -5,22 +5,16 @@ export function defineEventHandlerIDLAttribute<
 >(target: T, name: N) {
     const eventName = name.substring(2); // remove leading 'on'
     let value: EventListener | null = null;
-    let wrapper: EventListener | null = null;
     Object.defineProperty(target, name, {
         get() {
             return value;
         },
         set(newValue: EventListener | null) {
-            if (newValue === null || newValue === undefined) {
-                if (wrapper !== null) {
-                    this.removeEventListener(eventName, wrapper);
-                }
-                wrapper = null;
-            } else {
-                if (wrapper === null) {
-                    wrapper = (...args) => value!.apply(this, args);
-                    this.addEventListener(eventName, wrapper);
-                }
+            if (value === null || value === undefined) {
+                this.removeEventListener(eventName, value);
+            }
+            if (newValue !== null && newValue !== undefined) {
+                this.addEventListener(eventName, newValue);
             }
             value = newValue;
         },
