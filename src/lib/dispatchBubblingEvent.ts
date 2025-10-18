f728114c067298e3056585c118802c6816b463bb
Nolan Lawson <nolan@nolanlawson.com>
2025-10-18 13:59:06 -1000

    start on capture

--- src/lib/dispatchBubblingEvent.ts
+++ src/lib/dispatchBubblingEvent.ts
@@ -13,7 +13,42 @@ export function dispatchBubblingEvent(
         writable: false,
         value: target,
     });
+
+    // capture phase
+
+    Object.defineProperty(event, "eventPhase", {
+        writable: true,
+        configurable: true,
+        value: Event.CAPTURING_PHASE,
+    });
+
+    for (const ancestor of [...ancestors].reverse()) {
+        if (event._propagationStopped || event._immediatePropagationStopped) {
+            break;
+        }
+        Object.defineProperty(event, "currentTarget", {
+            writable: true,
+            configurable: true,
+            value: ancestor,
+        });
+        dispatchEventWithErrorHandling(ancestor, event);
+    }
+
+    // "at target" phase
+    Object.defineProperty(event, "eventPhase", {
+        writable: true,
+        configurable: true,
+        value: Event.AT_TARGET,
+    });
+    Object.defineProperty(event, "currentTarget", {
+        writable: true,
+        configurable: true,
+        value: target,
+    });
+
     dispatchEventWithErrorHandling(target, event);
+
+    // bubble phase
 
     Object.defineProperty(event, "eventPhase", {
         writable: true,
@@ -22,7 +57,7 @@ export function dispatchBubblingEvent(
     });
     for (const ancestor of ancestors) {
         if (event._propagationStopped || event._immediatePropagationStopped) {
-            return;
+            break;
         }
         Object.defineProperty(event, "currentTarget", {
             writable: true,
